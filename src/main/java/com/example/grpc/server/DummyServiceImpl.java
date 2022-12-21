6f7245a1e895093ef0f41cec750d5eb274154893
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-12-22 02:01:23 +0700

    Add files via upload

---
+++ src/main/java/com/example/grpc/server/DummyServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.grpc.server;
+
+
+import com.proto.greet.DummyMessage;
+import com.proto.greet.DummyServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class DummyServiceImpl extends DummyServiceGrpc.DummyServiceImplBase {
+    @Override
+    public void sayHi(DummyMessage request, StreamObserver<DummyMessage> responseObserver) {
+
+    // Block 1: extract the data required
+        String txt = request.getTxt();
+
+    // Block 2: create the response message
+        String result = "Hello " + txt;
+        DummyMessage response = DummyMessage.newBuilder().setTxt(result).build();
+
+    // Block 3: send the response
+        responseObserver.onNext(response);
+
+    // Block 4: complete the RPC call
+        responseObserver.onCompleted();
+    }
+}
